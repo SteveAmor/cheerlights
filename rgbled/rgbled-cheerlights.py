1d13d1691bd5b71af39ff7547ba4a69f3d9552ad
SteveAmor <SteveAmor@users.noreply.github.com>
2016-12-11 09:16:59 +0000

    Add rgb led code

---
+++ rgbled/rgbled-cheerlights.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from __future__ import division
+import time
+import requests
+from gpiozero import RGBLED
+
+col = "#000000"
+
+rgbled = RGBLED(17,27,22)
+
+while True:
+    try:
+        r = requests.get('http://api.thingspeak.com/channels/1417/field/2/last.json')
+        col = r.json()['field2']
+    except:
+        if col == "#000000":
+            col = "#FFFFFF"
+        else:
+            col = "#000000"
+    r, g, b = tuple(ord(c) for c in col[1:].lower().decode('hex'))
+    rgbled.color = (r/255, g/255, b/255)
+    time.sleep(1)
